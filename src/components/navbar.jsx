58ddde4e8c3006c50d841da0bc47a8ef8ae59beb
Deky Berniawan <dekyberniawan@gmail.com>
2024-01-23 16:14:26 +0700

    create project reactjs

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-light border-bottom border-success border-2 shadow fixed-top">
+      <div className="container-fluid">
+        <a href="#" className="navbar-brand fw-bolder text-success">
+          Kelas Food
+        </a>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link link-dark fw-semibold">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/list-menu"
+              className="nav-link link-dark fw-semibold ms-sm-5"
+            >
+              List Menu
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link-dark fw-semibold ms-sm-5">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
